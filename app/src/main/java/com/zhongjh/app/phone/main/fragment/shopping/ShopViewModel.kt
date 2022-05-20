5d4d7f0f2087bd4a8e017cc641a89eba875c54c3
zhongjh <254191389@qq.com>
2022-05-20 18:00:19 +0800

    完善框架

--- app/src/main/java/com/zhongjh/app/phone/main/fragment/shopping/ShopViewModel.kt
+++ app/src/main/java/com/zhongjh/app/phone/main/fragment/shopping/ShopViewModel.kt
@@ -3,16 +3,18 @@ package com.zhongjh.app.phone.main.fragment.shopping
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.asLiveData
+import androidx.lifecycle.viewModelScope
 import com.zhongjh.app.data.http.service.BannerApi
 import com.zhongjh.app.entity.ApiEntity
 import com.zhongjh.app.entity.Banner
 import com.zhongjh.mvvmibatis.extend.launchFlow
 import com.zhongjh.mvvmibatis.model.State
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.delay
 import javax.inject.Inject
 
 /**
- *
+ * https://github.com/shounakmulay/KotlinFlowTest/tree/part2-end
  * @author zhongjh
  * @date 2022/5/7
  */
@@ -28,6 +30,18 @@ class ShopViewModel @Inject constructor(
 
     var getBanner: LiveData<State<List<Banner>>> =
         bannerFlow.asLiveData()
+
+    val countFlow = mainRepository
+        .count3Flow()
+        .flowOn(dispatcherProvider.default)
+
+    val doubleCountFlow = countFlow.map {
+        delay(200)
+        it * 2
+    }.flowOn(dispatcherProvider.io)
+
+    val doubleCountSharedFlow =
+        doubleCountFlow.shareIn(viewModelScope, sharingStrategyProvider.eagerly)
 
 
     fun mockApiBannerList(value: List<Banner>): ApiEntity<List<Banner>> {
